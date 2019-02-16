fc3030519fa5d966eab4855abdf3cd666b928987
Coande <e12e@qq.com>
2019-02-16 22:49:24 +0800

    添加utils

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
+import utils from '@/utils'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+// 把 utils 注册到全局
+Vue.prototype.$utils = utils
 
 new Vue({
   render: h => h(App),
